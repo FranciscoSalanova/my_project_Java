311b0b663145298442d7aeebfaa63252d732ee0f
Francisco Salanova <fsalanova.16@gmail.com>
2022-01-23 12:33:43 -0300

    Agregado de validacion de teclas ingresadas en las cajas de texto

--- src/Conver.java
+++ src/Conver.java
@@ -25,72 +25,90 @@ public class Conver extends javax.swing.JFrame {
         // la siguiente línea genera la ventana de aplicación vacía, sin elementos:
         getContentPane().setLayout(null);
 
-        // de izquierda a derecha y de arriba hacia abajo, primero situamos el cartel de "Grados centigrados":
+        // definición de parámetros de la etiqueta que identifica la caja de grados centígrados:
         cartel_gradosC.setText("Grados centigrados");
         cartel_gradosC.setBounds(40, 28, 116, 16);
-
+        // agregado de la etiqueta para grados centígrados:
         getContentPane().add(cartel_gradosC);
 
-        // a la derecha del cartel anterior va la caja de texto donde se visualizará la cantidad de grados centigrados:
+        // definición de parámetros de la caja de texto para grados centígrados:
         caja_gradosC.setText("0.0");
         caja_gradosC.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         caja_gradosC.setBounds(180, 28, 144, 20);
-
+        // agregado de la caja de texto para grados centígrados:
         getContentPane().add(caja_gradosC);
 
-        // abajo de la sección de grados centigrados, empezamos con la etiqueda de "Grados Fahrenheit":
+        // definición de parámetros de la etiqueta que identifica la caja de grados fahrenheit:
         cartel_gradosF.setText("Grados fahrenheit");
         cartel_gradosF.setBounds(40, 75, 104, 24);
-
+        // agregado de la etiqueta para grados fahrenheit:
         getContentPane().add(cartel_gradosF);
 
-        // a la derecha de la etiqueta situamos la caja de texto correspondiente a grados fahrenheit:
+        // definición de parámetros de la caja de texto para grados fahrenheit:
         caja_gradosF.setText("32.0");
         caja_gradosF.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         caja_gradosF.setBounds(180, 75, 144, 20);
-
+        // agregado de la caja de texto para grados fahrenheit:
         getContentPane().add(caja_gradosF);
 
-        // luego de los carteles y las cajas de texto para los grados, incluimos el botón para la conversión:
-        boton_conversor.setText("Aceptar");
+        // definición de parámetros del botón que permitirá realizar la conversión entre medidas de temperatura:
+        boton_conversor.setText("Convertir");
         boton_conversor.setToolTipText("Pulse aquí para realizar la conversión");
         boton_conversor.setMnemonic('a');
         getRootPane().setDefaultButton(boton_conversor);
         boton_conversor.setBounds(180, 120, 144, 20);
-
+        // agregado del botón:
         getContentPane().add(boton_conversor);
+        
+        // definición del escuchador de ventana, que permite manipular la interfaz de eventos a nivel ventana:
         addWindowListener(new java.awt.event.WindowAdapter() {
             
+            // reescritura del método "windowOpened" para incluir la ejecución de nuestro método "formWindowOpened":
             public void windowOpened(java.awt.event.WindowEvent evt) {
                 
                 formWindowOpened(evt);
             }
             
-            // se define lo que el manejador de eventos debe controlar cuando se quiere cerrar la ventana.
+            // reescritura del método "windowClosing" para incluir la ejecución de nuestro método "exitForm":
             public void windowClosing(java.awt.event.WindowEvent evt) {
+                
                 exitForm(evt);
             }
         });
 
-        // se asocia el método a ejecutar con el escuchador de acción que estará pendiente de que se oprima la tecla "Enter".
+        // definición del escuchador de acciones, que permite responder con métodos específicos a eventos de acción sobre componentes:
         java.awt.event.ActionListener al = new java.awt.event.ActionListener() {
 
+            // reescritura del método "actionPerformed" para incluir la ejecución del método propio "teclaEnter":
             public void actionPerformed(java.awt.event.ActionEvent evt) {
 
                 teclaEnter(evt);
             }
         };
-
+        // vinculación del escuchador con las cajas de texto, aquellos componenetes que responderán al presionado de la tecla "Enter":
         caja_gradosC.addActionListener(al);
         caja_gradosF.addActionListener(al);
+
+        // definición del escuchador de teclas, que permite responder a los mensajes vinculados al presionado de teclas:
         java.awt.event.KeyAdapter kl = new java.awt.event.KeyAdapter() {
 
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+
+                teclaPresionada(evt);
+            }
+            
+            // reescritura del método "KeyTyped" para incluir la ejecución del método propio "teclaPresionada":
             public void keyTyped(java.awt.event.KeyEvent evt) {
                 
-                teclaPresionadaCaja(evt);
+                teclaPresionada(evt);
+            }
+
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+
+                teclaLiberada(evt);
             }
         };
-
+        // vinculación del escuchador con las cajas de texto, aquellos componenetes que responderán al presionado de la teclas:
         caja_gradosC.addKeyListener(kl);
         caja_gradosF.addKeyListener(kl);
         
@@ -105,7 +123,7 @@ public class Conver extends javax.swing.JFrame {
         caja_gradosC.addFocusListener(fl);
         caja_gradosF.addFocusListener(fl);
 
-        // definimos el comportamiento del botón al hacerse click sobre él:
+        
         boton_conversor.addActionListener(new java.awt.event.ActionListener() {
 
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -115,13 +133,71 @@ public class Conver extends javax.swing.JFrame {
         });
     }
 
+    private void teclaEnter(java.awt.event.ActionEvent evt) {
+
+        try {
+            double grados = 0.0;
+            String sGrados = "";
+            Object obj_caja = evt.getSource();
+            
+            // en caso de que el usuario ingrese un dato en la caja de grados centígrados:
+            if (obj_caja == caja_gradosC) {
+                sGrados = caja_gradosC.getText();
+                if (sGrados.length() == 0) {
+                    return;
+                }
+                grados = (Double.parseDouble(caja_gradosC.getText()) * 9.0 / 5.0) + 32.0;
+                String texto = String.format("%.2f", grados);
+                caja_gradosF.setText(texto);
+            }
+
+            // en caso de que el usuario ingrese un dato en la caja de grados fahrenheit:
+            if (obj_caja == caja_gradosF) {
+                sGrados = caja_gradosF.getText();
+                if (sGrados.length() == 0) {
+                    return;
+                }
+                grados = (Double.parseDouble(caja_gradosF.getText()) - 32.0) * 5.0 / 9.0;
+                String texto = String.format("%.2f", grados);
+                caja_gradosC.setText(texto);
+            }
+        }
+        catch (NumberFormatException e) {
+            caja_gradosC.setText("0.0");
+            caja_gradosF.setText("32.0");
+        }
+    }
+
+    private void teclaLiberada(java.awt.event.KeyEvent evt) {
+
+        javax.swing.JTextField objJTF = (javax.swing.JTextField)evt.getSource();
+        String str = objJTF.getText();
+        char[] fuente = str.toCharArray();
+        char[] resultado = new char[fuente.length];
+        int j = 0;
+        boolean error = false;
+        
+        for (int i = 0; i < fuente.length; i++) {
+            if (fuente[i] >= '0' && fuente[i] <= '9' || fuente[i] == '.' || fuente[i] == '+' || fuente[i] == '-') {
+                resultado[j++] = fuente[i];
+            }
+            else {
+                error = true;
+                java.awt.Toolkit.getDefaultToolkit().beep();
+            }
+        }
+        if (error) {
+            objJTF.setText(new String(resultado, 0, j));
+        }
+    }
+
     private void textoSeleccionadoCaja(java.awt.event.FocusEvent evt) {
 
         javax.swing.JTextField objEnfocado = (javax.swing.JTextField)evt.getSource();
         objEnfocado.selectAll();
     }
 
-    private void teclaPresionadaCaja(java.awt.event.KeyEvent evt) {
+    private void teclaPresionada(java.awt.event.KeyEvent evt) {
         
         obj_caja = evt.getSource();
     }
@@ -162,40 +238,6 @@ public class Conver extends javax.swing.JFrame {
         System.exit(0);
     }
 
-    private void teclaEnter(java.awt.event.ActionEvent evt) {
-
-        try {
-            double grados = 0.0;
-            String sGrados = "";
-            Object obj_caja = evt.getSource();
-            
-            // en caso de que el usuario ingrese un dato en la caja de grados centígrados:
-            if (obj_caja == caja_gradosC) {
-                sGrados = caja_gradosC.getText();
-                if (sGrados.length() == 0) {
-                    return;
-                }
-                grados = (Double.parseDouble(caja_gradosC.getText()) * 9.0 / 5.0) + 32.0;
-                String texto = String.format("%.2f", grados);
-                caja_gradosF.setText(texto);
-            }
-
-            // en caso de que el usuario ingrese un dato en la caja de grados fahrenheit:
-            if (obj_caja == caja_gradosF) {
-                sGrados = caja_gradosF.getText();
-                if (sGrados.length() == 0) {
-                    return;
-                }
-                grados = (Double.parseDouble(caja_gradosF.getText()) - 32.0) * 5.0 / 9.0;
-                String texto = String.format("%.2f", grados);
-                caja_gradosC.setText(texto);
-            }
-        }
-        catch (NumberFormatException e) {
-            caja_gradosC.setText("0.0");
-            caja_gradosF.setText("32.0");
-        }
-    }
     public static void main(String[] args) {
         
         // definimos el L&F de la ventana de la aplicación:
